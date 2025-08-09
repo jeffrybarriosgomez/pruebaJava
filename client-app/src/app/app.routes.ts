2a8108f35fb34ef425e5233f992a0d5d323b176c
Jeffry Barrios <jeffrybarriosgomez@gmail.com>
2025-08-09 16:03:38 -0500

    config angular proxy v2

--- client-app/src/app/app.routes.ts
+++ client-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PlaylistList } from '../app/components/playlist-list/playlist-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: PlaylistList },
+];
+
